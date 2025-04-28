ae2141179e4b348bb608de1938c095c5f9ce8c51
Malik <hafizdaniyalshakeel@gmail.com>
2025-04-29 00:56:41 +1000

    Day 1

---
+++ index.js
@@ -0,0 +1,121 @@
+// var a;
+// var, let const
+// var let (hoisting)
+
+// let a = 10;
+// const b = 29;
+// b = 20;
+// console.log(a)
+// console.log(a)
+// a = 10;
+// let a = 10;
+// let b = 2;
+// console.log(a ** b); // a ^ b
+
+// ++a, a++
+// let a = 10;
+// console.log(++a); // 11
+// console.log(a++); // 10
+// let a = "10";
+// let b = 20;
+// console.log(a === b)
+
+// console.log( a === 10 ? "Sahi hai " : "Galat hai")
+// function obj() {
+//   return { arr: ["10"] };
+// }
+// console.log(typeof null);
+
+// Primitive, 
+// 1. Number, String, Null, Undefined, Boolean
+//  Non primitive
+// 2. Object, Array, Function
+
+// let a = 10;
+// let b = a;
+// b = 20;
+// console.log(a);
+
+// let obj1 = {
+//     "name": "Daniyal"
+// }
+
+// let obj2 = {
+//     "name": "Daniyal"
+// }; 
+// obj2.age = 20;
+
+// console.log(obj1)
+
+// function greet(){
+//     console.log("greeting")
+// }
+
+// greet();
+
+// let car = {
+//     start: function () {
+//         return "Engine Started..."
+//     }
+// }
+
+// console.log(car.start())
+
+// let person1 = {};
+// let person2 = new Object();
+// let arr = new Array() // []
+// let str = new String() // ""
+// let date = new Date() // UTC format
+
+// person1.name = "Daniyal"
+// person2.name = "Talha"
+
+// console.log(person1)
+// console.log(person2)
+
+// Object Properties
+// let obj = {
+//     name: "Daniyal", //[name: "Daniyal"]
+//     age: 20,// [age: 20]
+//     sex: "male"
+// }
+
+// console.log(Object.keys(obj))
+// console.log(Object.values(obj))
+// console.log(Object.entries(obj))
+
+// Function length depends on parameters
+
+// function sum(a, b, c){
+//     return a+b+c
+// }
+// console.log(sum.length)
+
+// Strings
+// let str1 = "Hell'o";
+// "It's my first job"
+// '"John Snow"'
+// let str2 = 'Jaaan';
+
+// let person = {
+//     name: "Daniyal",
+//     age: 20
+// }
+
+// String literal (Backticks)
+// let str3 = `My name is ${person.name} and i am ${person.age} years old`;
+// console.log(str3)
+
+// Trim
+// "    hello   "
+// "hello"
+
+// let url = "https://www.w3schools.com/js/js_string_methods.asp"
+// console.log(url.split('/')); // return array
+
+
+
+// -------------------------------------------Day 1---------------------------------------------------------
+
+
+// Destructuring, Spread Operator, Rest Operator, forEach, for/in, for/of, map(), filter(), find()
